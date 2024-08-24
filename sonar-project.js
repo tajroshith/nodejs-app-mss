beb5986c4ae6fa5d707baaa7d44e349c3fa8eeb1
Taj Roshith <88622844+tajroshith@users.noreply.github.com>
2024-08-24 16:18:11 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.39.184:9000/',
+  serverUrl: 'http://172.31.12.188:9980/taj',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_ee85157f1f6559cc393a592a783ee7676ba3ac2d',
+	    'sonar.login': 'squ_eebd4c075a1c80f16e090881786225ca7af899d8',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
